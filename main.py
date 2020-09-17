fa7dab5f84f0eb7adee6e7a5357ac6a24e580298
Insan Kamil <insankamil002@gmail.com>
2020-09-17 20:21:26 +0000

    add main.py file

--- main.py
+++ main.py
@@ -178,7 +178,6 @@ for i in range(0,25):
     cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
 
 
-
 geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
 
 
